96910a24a5b343dedb34cd9cc231464325f4e4fa
Danylo_Mykhed <danya.mykhed@gmail.com>
2025-05-21 17:49:50 +0300

    Add category_detail_view function

--- recipe/views.py
+++ recipe/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
-from .models import Recipe # Імпорт вашої моделі Recipe
+from django.shortcuts import render, get_object_or_404
+from .models import Recipe, Category
 import random
 
-def main_view(request): # Назва view згідно завдання: main
+def main_view(request):
     all_recipes_list = list(Recipe.objects.all())
 
     if len(all_recipes_list) >= 10:
@@ -14,3 +14,12 @@ def main_view(request): # Назва view згідно завдання: main
         'recipes': random_recipes
     }
     return render(request, 'recipe/main.html', context)
+def category_detail_view(request, category_id):
+    category = get_object_or_404(Category, pk=category_id)
+    recipes_in_category = category.categories.all()
+
+    context = {
+        'category': category,
+        'recipes': recipes_in_category,
+    }
+    return render(request, 'recipe/category_detail.html', context)
