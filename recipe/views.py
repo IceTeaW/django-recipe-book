ea29d4cd5b7fdcda4af73b53687dbec944de3176
Danylo_Mykhed <danya.mykhed@gmail.com>
2025-05-21 17:41:21 +0300

    Add main_view function for random recipes

--- recipe/views.py
+++ recipe/views.py
@@ -1,1 +1,16 @@
+from django.shortcuts import render
+from .models import Recipe # Імпорт вашої моделі Recipe
+import random
 
+def main_view(request): # Назва view згідно завдання: main
+    all_recipes_list = list(Recipe.objects.all())
+
+    if len(all_recipes_list) >= 10:
+        random_recipes = random.sample(all_recipes_list, 10)
+    else:
+        random_recipes = all_recipes_list
+
+    context = {
+        'recipes': random_recipes
+    }
+    return render(request, 'recipe/main.html', context)
